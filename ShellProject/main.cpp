1e6c7998efaa8b14aef8160efd7670f624ec004f
Malone, Adem (S&T-Student) <amrf3@mst.edu>
2019-11-04 17:31:11 +0000

    Upload New File

---
+++ ShellProject/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Name: Cash 
+ * Class: Operating Systems
+ * Assignment Shell Emulator
+ */
+
+#include <iostream>
+#include "computer.h"
+#include "node.h"
+
+int main()
+{
+  // Make a computer
+  Shell::Computer c;
+  // run it.
+  c.run();
+
+  return 0;
+}
